995c2ca6d1cc467fce3021babd7f9ad3cbedd65a
divashka <dianakuzmina325@gmail.com>
2024-04-09 15:42:35 +0300

    Верстка каталога

--- src/pages/catalog/catalog.jsx
+++ src/pages/catalog/catalog.jsx
@@ -3,21 +3,27 @@ import HeaderMain from "../../components/header-main/header-main";
 import CardList from "../../components/card-list/card-list";
 import './catalog.css';
 import { products } from "../../mock/mock";
+import Filter from "../../components/filter/filter";
+import Footer from "../../components/footer/footer";
 
 function CatalogPage() {
  return (
   <>
    <HeaderMain />
 
-   <main>
+   <main className="page">
 
     <div className="container">
+
+     <Filter />
 
      <CardList products={products} />
 
     </div>
 
    </main >
+
+   <Footer />
 
   </>
  )
