1d8a396481410eb0a6f13f4e9366a4defb9f45d9
divashka <dianakuzmina325@gmail.com>
2024-04-09 15:42:50 +0300

    Поравила стили и верстку main

--- src/pages/main/main.jsx
+++ src/pages/main/main.jsx
@@ -4,76 +4,85 @@ import HeaderMain from "../../components/header-main/header-main";
 import CardList from "../../components/card-list/card-list";
 import './main.css';
 import { productsTop } from "../../mock/mock";
+import SubscribeForm from "../../components/subscribe/subscribe";
+import Footer from "../../components/footer/footer";
 
 function MainPage() {
- return (
-  <>
-   <HeaderMain />
+  return (
+    <>
+      <HeaderMain />
 
-   <main>
+      <main>
 
-    <div className="slider">
-     <picture>
-      <source type="image/png" media="(max-width: 600px)" srcset="img/slides/slide_1@600.png" />
-      <source type="image/png" media="(max-width: 1024px)"
-       srcset="img/slides/slide_1@1280.png" />
-      <img className="slide__img" src="img/slides/slide_1@1920.png" srcSet="img/slides/slide_1@1920.png"
-       alt="Slide 1" />
-     </picture>
-     <div className="slider__info info">
-      <p className="info__text">
-       <span className="info__title">
-        <span className="info__logo">MARESA SKINCARE</span>
-        Сердце океана в каждом средстве
-       </span>
-       <button type="button" className="button info__button">В каталог</button>
-      </p>
-      <picture>
-       <source type="image/png" media="(max-width: 600px)" srcset="img/slides/min_1@600.png" />
-       {/* <source type="image/png" media="(max-width: 1024px)"
-        srcset="img/hero-tablet.png, img/hero-tablet@2x.png 2x" /> */}
-       <img className="info__img" src="img/slides/min_1@1920.png" srcSet="img/slides/min_1@1920.png"
-        alt="Min 1" />
-      </picture>
-     </div>
-    </div>
+        <div className="slider">
 
-    <section className="popular__catalog">
-     <div className="popular__container container">
+          <picture>
+            <source type="image/png" media="(max-width: 600px)" srcSet="img/slides/slide_1@600.png 1x" />
+            <source type="image/png" media="(max-width: 1280px)"
+              srcSet="img/slides/slide_1@1280.png" />
+            <img className="slide__img" src="img/slides/slide_1@1920.png" srcSet="img/slides/slide_1@1920.png"
+              alt="Slide 1" />
+          </picture>
 
-      <h1 className="visually-hidden">Профессиональная косметика по уходу за кожей лица и волосами</h1>
-      <span className="popular__title title">Топ 3 популярных товара для ежедневного ухода</span>
+          <div className="slider__info info">
+            <p className="info__text">
+              <span className="info__title">
+                <span className="info__logo">MARESA SKINCARE</span>
+                Сердце океана в каждом средстве
+              </span>
+              <button type="button" className="button info__button">В каталог</button>
+            </p>
+            <picture>
+              <source type="image/png" media="(max-width: 600px)" srcSet="img/slides/min_1@600.png" />
+              <img className="info__img" src="img/slides/min_1@1920.png" srcSet="img/slides/min_1@1920.png"
+                alt="Min 1" />
+            </picture>
+          </div>
+        </div>
 
-      <CardList products={productsTop} />
+        <section className="popular__catalog">
+          <div className="popular__container container">
 
-     </div>
-    </section>
+            <h1 className="visually-hidden">Профессиональная косметика по уходу за кожей лица и волосами</h1>
+            <span className="popular__title title">Топ 3 популярных товара для ежедневного ухода</span>
 
-    <section className="about">
-     <div className="about__container container">
+            <CardList products={productsTop} />
 
-      <h2 className="visually-hidden">Происхождение имени бренда</h2>
+          </div>
+        </section>
 
-      <div className="about__inner">
-       <div className="about__column">
-        <p className="about__text">
-         Бренд MARESA SKINCARE был основан в 2017 году. Название образовано от словосочетаний <span className="about__mark">SApare del MARE</span> и <span className="about__mark">SApоre de MARE</span>, что в переводе с итальянского означает «знание моря» и «вкус моря».
-        </p>
-        <a href="#" className="about__link">Подробнее о бренде</a>
-       </div>
+        <section className="about">
+          <div className="about__container container">
 
-       <div className="about__column">
-        <img className="about__img" src="./src/assets/about.png" alt="Подробнее о бренде" width="500" height="428" />
-       </div>
-      </div>
+            <h2 className="visually-hidden">Происхождение имени бренда</h2>
 
-     </div>
-    </section>
+            <div className="about__inner">
+              <div className="about__column">
+                <p className="about__text">
+                  Бренд MARESA SKINCARE был основан в 2017 году. Название образовано от словосочетаний <span className="about__mark">SApare del MARE</span> и <span className="about__mark">SApоre de MARE</span>, что в переводе с итальянского означает «знание моря» и «вкус моря».
+                </p>
+                <a href="#" className="about__link">Подробнее о бренде</a>
+              </div>
 
-   </main >
+              <div className="about__column">
+                <picture>
+                  <source type="image/png" media="(max-width: 600px)" srcSet="img/about@600.png" />
+                  <img className="about__img" src="img/about@1920.png" srcSet="img/about@1920.png"
+                    alt="Подробнее о бренде" />
+                </picture>
+              </div>
+            </div>
 
-  </>
- )
+          </div>
+        </section>
+
+        <SubscribeForm />
+
+      </main >
+
+      <Footer />
+    </>
+  )
 }
 
 export default MainPage;
