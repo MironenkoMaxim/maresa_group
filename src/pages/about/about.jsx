6536a0fbf34418f2d036973465ae2ed7f9b980b2
divashka <dianakuzmina325@gmail.com>
2024-04-10 14:10:44 +0300

    Верстка страницы о бренде

--- src/pages/about/about.jsx
+++ src/pages/about/about.jsx
@@ -1,18 +1,42 @@
 import HeaderMain from "../../components/header-main/header-main";
 import Footer from "../../components/footer/footer";
+import "./about.css";
 
 function AboutPage() {
  return (
   <>
    <HeaderMain />
 
-   <main>
+   <main className="page page_about-brand">
 
-    <div className="container-main">
+    <section className="about-brand">
+     <div className="about-brand__container container">
+      <div className="about-brand__hero">
+       <div className="about-brand__column about-brand__column_text">
+        <span className="about-brand__sub">Основан в 2017 году</span>
+        <div className="about-brand__logo logo">
+         <span className="logo__sub">MARESA</span>
+         <span className="logo__group">GROUP</span>
+        </div>
+        <span className="about-brand__text">Бренд профессиональной косметики по уходу за кожей</span>
+       </div>
+       <div className="about-brand__product-img">
+        <picture>
+         <source type="image/png" media="(max-width: 600px)" srcSet="img/about-min@600.png" />
+         <img className="about-brand__img" src="img/about@1920.png" srcSet="img/about-min@1920.png"
+          alt="Подробнее о бренде" />
+        </picture>
+       </div>
+       <div className="about-brand__column about-brand__column_img"></div>
+      </div>
+     </div>
 
-     <h1>About</h1>
-
-    </div>
+     <div className="about-brand__container container">
+      <p className="about-brand__main-text">
+       MARESA SKINCARE СОЗДАЕТ ПРОДУКТЫ, КОТОРЫЕ ЭФФЕКТИВНО УДОВЛЕТВОРЯЮТ ЕЖЕДНЕВНЫЕ ПОТРЕБНОСТИ В УХОДЕ ЗА КОЖЕЙ, СОХРАНЯЯ ЕЕ ЗДОРОВЬЕ И ЕСТЕСТВЕННУЮ КРАСОТУ.
+      </p>
+     </div>
+    </section>
 
    </main >
 
