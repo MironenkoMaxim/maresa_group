cba91f9d67fb97cfd1e7b880afd725365b78f51e
divashka <dianakuzmina325@gmail.com>
2024-04-12 19:09:10 +0300

    Поправила работу карточек товара

--- src/components/card-item/card-item.jsx
+++ src/components/card-item/card-item.jsx
@@ -1,26 +1,39 @@
 import './card-item.css';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { AppRoute } from '../../const';
 
 function CardItem(props) {
 
- const { id, name, url, price } = props.product;
+ const { id, name, url, price, volume } = props.product;
 
- function handleButtonClick() {
-  console.log('нажал кнопку')
+ const navigate = useNavigate();
+
+ function handleCatalogItemClick() {
+  navigate(`${AppRoute.product}/${id}`);
  }
 
  return (
   <li className="catalog__item">
 
-   <div className="catalog__preview-wrapper">
+   <div className="catalog__preview-wrapper" onClick={handleCatalogItemClick}>
     <img src={url} className="catalog__preview" alt="Карточка товара" />
    </div>
 
    <h3 className='catalog__title'>{name}</h3>
 
+   <ul className="catalog__volume catalog-volume">
+    {
+     volume.map((value, index) => (
+      <li key={value} className='catalog-volume__item'>
+       {value} мл
+       {index + 1 !== volume.length && <span className='catalog-volume__del'>|</span>}
+      </li>
+     ))
+    }
+   </ul>
+
    <div className="catalog__bottom">
-    <Link className="catalog__link" to={`${AppRoute.product}/${id}`}>Подробнее
+    <Link className="catalog__link link" to={`${AppRoute.product}/${id}`}>Подробнее
     </Link>
     <span className="catalog__price">{price} руб</span>
    </div>
