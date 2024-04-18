1b7816b8c463b7e87d82cf19d37ec7f918b927e2
divashka <dianakuzmina325@gmail.com>
2024-04-18 18:26:59 +0300

    Поправила кнопку в not-found

--- src/pages/not-found/not-found.jsx
+++ src/pages/not-found/not-found.jsx
@@ -2,6 +2,8 @@ import HeaderMain from "../../components/header-main/header-main";
 import Footer from "../../components/footer/footer";
 import "./not-found.css";
 import { Helmet } from "react-helmet-async";
+import { AppRoute } from "../../const";
+import { Link } from "react-router-dom";
 
 function NotFoundPage() {
  return (
@@ -18,10 +20,10 @@ function NotFoundPage() {
      <div className="hero__content">
       <h1 className="hero__title"><span className="hero__error">404</span> Страница не найдена </h1>
       <p className="hero__text">Возможно ваша ссылка устарела</p>
-      <button type="button" className="hero__button border__button border__button_light">Перейти на главную</button>
+      <Link type="button" to={AppRoute.root} className="hero__button border__button border__button_light">Перейти на главную</Link>
      </div>
     </section>
-    
+
    </main >
 
    <Footer />
